ada6de850ebd2505a426c6d427aa028e10ea2ffa
kzlsakal <dorukgurleyen@gmail.com>
2020-09-14 12:23:26 -0700

    Add styled components dependency

--- client/components/Talia.jsx
+++ client/components/Talia.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
 
- const Talia = () => {
+const Styled = {
+  Global: createGlobalStyle`
+    body {
+      background-color: rgb(40, 42, 55);
+      box-style: border-box;
+      color: rgb(190, 195, 205);
+      font-family: monospace, sans-serif;
+      font-size: 32px;
+      font-size: 2vmax;
+      height: 100%;
+      margin: 0;
+      padding:
+      position: relative;
+      width: 100%;
+    }
+  `
+};
+
+const Talia = () => {
   return (
     <div>
-      greetings from Talia!
+      <Styled.Global />
+        greetings from Talia!
     </div>
   );
 };
